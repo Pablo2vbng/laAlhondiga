5846ef3fde4bd9a74bbfc5c2c18a769a59b06037
Pablo Vidal Vidal <pablo2vbngdaw@gmail.com>
2026-01-04 12:23:56 +0100

    primer push

---
+++ js/audio-control.js
@@ -0,0 +1,9 @@
+const aud = document.getElementById("audioEslogan");
+
+function playAudio() {
+    aud.play();
+}
+
+function pauseAudio() {
+    aud.pause();
+}
---
+++ js/video-control.js
@@ -0,0 +1,9 @@
+const vid = document.getElementById("videoMercado");
+
+function playVideo() {
+    vid.play();
+}
+
+function pauseVideo() {
+    vid.pause();
+}
